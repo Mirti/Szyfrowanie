556ec562a561fd242c4d2b9aa7c7e888f22e37b5
anykiel <kijoszeq12@gmail.com>
2018-05-21 12:53:54 +0200

    dodaje md5

--- src/szyfrowanie/MD5SceneController.java
+++ src/szyfrowanie/MD5SceneController.java
@@ -5,33 +5,15 @@
  */
 package szyfrowanie;
 
-import java.io.UnsupportedEncodingException;
 import java.net.URL;
-import java.nio.charset.Charset;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
-import org.omg.IOP.Encoding;
 
 /**
  * FXML Controller class
@@ -73,58 +55,27 @@ public class MD5SceneController implements Initializable {
     */
     @FXML
     private void process() throws Exception{
-        if(rbEncode.isSelected()) {
             encode();
-        }
-        else {
-            decode();
-        }
     }
     
     //Metoda szyfrująca podanym kluczem
     @FXML
      public void encode() throws Exception
     {
-        String message = txtInput.getText();
-        final MessageDigest digmsg = MessageDigest.getInstance("md5");
-        final byte[] passDig = digmsg.digest("HG58YZ3CR9".getBytes("utf-8"));
-        final byte[] byteArr = Arrays.copyOf(passDig, 24);
-        for (int j = 0, k = 16; j < 8;) {
-                byteArr[k++] = byteArr[j++];
-        }
+        String message = txtInput.getText();    
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(message.getBytes());
         
-        final SecretKey mdkey = new SecretKeySpec(byteArr, "DESede");
-        final IvParameterSpec iv = new IvParameterSpec(new byte[8]);
-        final Cipher szyfr = Cipher.getInstance("DESede/CBC/PKCS5Padding");
-        szyfr.init(Cipher.ENCRYPT_MODE, mdkey, iv);
-
-        final byte[] arrNiekod = message.getBytes("utf-8");
-        final byte[] arrKod = szyfr.doFinal(arrNiekod);
-        arr = arrKod;
-        txtResult.setText(arrKod.toString());
+        byte byteData[] = md.digest();
+        StringBuffer output = new StringBuffer();
+    	for (int i=0;i<byteData.length;i++) {
+    		String hex=Integer.toHexString(0xff & byteData[i]);
+   	     	if(hex.length()==1) output.append('0');
+   	     	output.append(hex);
+        }
+        txtResult.setText(output.toString());
     } 
 
-    //Metoda deszyfrująca podanym kluczem
-    @FXML
-    public void decode() throws Exception {
-        String message = txtInput.getText();
-        final MessageDigest digmsg = MessageDigest.getInstance("md5");
-        final byte[] passDig = digmsg.digest("HG58YZ3CR9".getBytes("utf-8"));
-        final byte[] byteArr = Arrays.copyOf(passDig, 24);
-        for (int j = 0, k = 16; j < 8;) {
-                byteArr[k++] = byteArr[j++];
-        }
-
-        final SecretKey mdkey = new SecretKeySpec(byteArr, "DESede");
-        final IvParameterSpec iv = new IvParameterSpec(new byte[8]);
-        final Cipher szyfr = Cipher.getInstance("DESede/CBC/PKCS5Padding");
-        szyfr.init(Cipher.DECRYPT_MODE, mdkey, iv);
-
-        final byte[] decodedArr = szyfr.doFinal(arr);
-
-        String output =  new String(decodedArr, "utf-8"); 
-        txtResult.setText(output);
-    }
     /*
     Metoda walidująca poprawność klucza.
      */
